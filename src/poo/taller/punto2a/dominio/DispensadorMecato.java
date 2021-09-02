550799f82d595c83aa7fb7d77d8499fb3b757d9a
MiguelRiosT <miguelriostangarife@gmail.com>
2021-09-02 02:31:08 -0500

    Se han creado la estructura pertinente de las clases punto2a

--- src/poo/taller/punto2a/dominio/DispensadorMecato.java
+++ src/poo/taller/punto2a/dominio/DispensadorMecato.java
@@ -1,5 +1,56 @@
 package poo.taller.punto2a.dominio;
 
-public class DispensadorMecato {
+import java.util.List;
 
+public class DispensadorMecato {
+    //Atributos
+    private String marca;
+    private Mecato productos;
+
+    //Constructor
+    public DispensadorMecato(Mecato productos) {
+        this.productos = productos;
+    }
+
+    //Operaciones
+    private boolean sacarUnidadNombre(){
+        return false;
+    }
+
+    private boolean sacarUnidadCodigo(){
+        return false;
+    }
+
+    //FALTA ESTE PROCESO CON LISTA
+   // private List<String> consultarAgotados(){
+    //return String;
+    //}
+
+    private int consultarUnidadesDeProducto(){
+        return 0;
+    }
+
+    private int consultarTotalUnidades(){
+        return 0;
+    }
+
+    private int aumentarStock(){
+        return 0;
+    }
+
+
+    //Getter
+    public String getMarca() {
+        return marca;
+    }
+
+    public Mecato getProductos() {
+        return productos;
+    }
+
+    //Setter
+
+    public void setProductos(Mecato productos) {
+        this.productos = productos;
+    }
 }
--- src/poo/taller/punto2a/dominio/Mecato.java
+++ src/poo/taller/punto2a/dominio/Mecato.java
@@ -1,8 +1,43 @@
 package poo.taller.punto2a.dominio;
 
 public class Mecato {
+    //Atributos
     private String nombre;
     private int codigo;
     private int precio;
     private int cantidadActual;
+
+    //Constructor
+    public Mecato(String nombre, int codigo) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+    }
+
+    //Genero los Getter
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public int getCantidadActual() {
+        return cantidadActual;
+    }
+
+    //Genero los Setter
+
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public void setCantidadActual(int cantidadActual) {
+        this.cantidadActual = cantidadActual;
+    }
 }
