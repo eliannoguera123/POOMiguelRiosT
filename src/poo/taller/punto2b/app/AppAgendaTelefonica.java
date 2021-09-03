37d23e9a14e0b882446e69d0f5c1a8e4ea3e7400
MiguelRiosT <miguelriostangarife@gmail.com>
2021-09-03 14:49:04 -0500

    Se crearon las estructuras en java del dominio y el app de los puntos 2a y 2b del taller, falta logica

---
+++ src/poo/taller/punto2b/app/AppAgendaTelefonica.java
@@ -0,0 +1,7 @@
+package poo.taller.punto2b.app;
+
+public class AppAgendaTelefonica {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/poo/taller/punto2b/dominio/AgendaTelefonica.java
@@ -0,0 +1,60 @@
+package poo.taller.punto2b.dominio;
+
+import java.util.List;
+
+public class AgendaTelefonica {
+    private int cantidadTotalContactos;
+    private List<Contacto> contactosFavoritos;
+
+
+    public AgendaTelefonica(int cantidadTotalContactos, List<Contacto> contactosFavoritos) {
+        this.cantidadTotalContactos = cantidadTotalContactos;
+        this.contactosFavoritos = contactosFavoritos;
+    }
+
+
+
+    //public Contacto crearNuevoContacto(){
+
+    //    return Contacto;
+   // }
+
+    public boolean eliminarContacto(){
+        return true;
+    }
+
+    public boolean buscarContactoNombre(){
+        return true;
+    }
+    public boolean buscarContactoApellido(){
+        return true;
+    }
+    public boolean buscarContactoNumero(){
+        return true;
+    }
+
+    public int cambiarNumeroContacto(){
+        return 0;
+    }
+
+    //public List<Contacto> ordenAlfabetico(){
+       // return List<Contacto>;
+    //}
+
+
+    public int getCantidadTotalContactos() {
+        return cantidadTotalContactos;
+    }
+
+    public List<Contacto> getContactosFavoritos() {
+        return contactosFavoritos;
+    }
+
+    public void setCantidadTotalContactos(int cantidadTotalContactos) {
+        this.cantidadTotalContactos = cantidadTotalContactos;
+    }
+
+    public void setContactosFavoritos(List<Contacto> contactosFavoritos) {
+        this.contactosFavoritos = contactosFavoritos;
+    }
+}
