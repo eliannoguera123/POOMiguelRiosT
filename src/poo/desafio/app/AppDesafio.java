797a342d3efadd29a4653651e83d50afc91b1f35
MiguelRiosT <miguelriostangarife@gmail.com>
2021-09-25 18:39:46 -0500

    Desafio calculadora progreso 6:39pm sabado

--- src/poo/desafio/app/AppDesafio.java
+++ src/poo/desafio/app/AppDesafio.java
@@ -1,29 +1,39 @@
 package poo.desafio.app;
 import poo.desafio.dominio.Calculadora;
-
 import javax.swing.*;
 import java.util.Arrays;
 
 public class AppDesafio {
-    private static ImageIcon icono = new ImageIcon("C:/Users/HG/MiguelRiosTPOO/calculadoraicono.png");
-
-
+    private static ImageIcon icono = new ImageIcon("C:/Users/HG/MiguelRiosTPOO/uco.png");
+    private static final int OPCION_SUMAR = 0;
+    private static final int OPCION_RESTAR = 1;
+    private static final int OPCION_MULTIPLICAR = 2;
+    private static final int OPCION_DIVIDIR = 3;
 
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora("LG");
-
         while(true){
-
             int opcionElegida = JOptionPane.showOptionDialog(null, "Elije una opcion",
                     "Calculadora ", 0, 0, icono,
-                    Arrays.asList("Sumar", "Restar", "Multiplicar", "Dividir").toArray(), "sumar");
+                    Arrays.asList("Sumar", "Restar", "Multiplicar", "Dividir").toArray(), null);
+
+            switch (opcionElegida) {
+                case OPCION_SUMAR :
+                    calculadora.sumar();
+                    break;
+                case OPCION_RESTAR :
+                    calculadora.restar();
+                    break;
+                case OPCION_MULTIPLICAR :
+                    calculadora.multiplicar();
+                    break;
+                case OPCION_DIVIDIR :
+                    calculadora.dividir();
+                    break;
+                case JOptionPane.CLOSED_OPTION:
+                    return;
+            }
         }
-
-
-
-
-
+        }
     }
 
-
-}
--- src/poo/desafio/dominio/Calculadora.java
+++ src/poo/desafio/dominio/Calculadora.java
@@ -1,47 +1,94 @@
 package poo.desafio.dominio;
-
-import poo.parcial.dominio.Producto;
+import java.util.List;
+import javax.swing.*;
 
 public class Calculadora {
+    private static ImageIcon icono = new ImageIcon("C:/Users/HG/MiguelRiosTPOO/uco.png");
     private String marcaCalculadora;
     private int numero;
+    private int n;
     private int resultado;
-
 
     public Calculadora(String marcaCalculadora) {
         this.marcaCalculadora = marcaCalculadora;
 
     }
 
-
-    //metodos
-
-
+    //METODOS
+    //INGRESAR NUMERO
     public int ingresarnumero(){
-        return 0;
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero: "));
+        return numero;
+    }
+    //MOSTRAR NUMERO
+    private static void mostrarnumero(int numero){
+        JOptionPane.showMessageDialog(null,numero,"Calculadora",JOptionPane.QUESTION_MESSAGE,icono);
     }
 
+
+    //SUMAR N NUMEROS
     public int sumar(){
-
-        return 0;
+        int sumaT=0;
+        n = Integer.parseInt(JOptionPane.showInputDialog("Ingrese n "));
+        for(int i=0;i<n;i++){
+           // numero=Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero"));//bien
+            numero = ingresarnumero();
+            sumaT = sumaT+numero;
+        }
+        JOptionPane.showMessageDialog(null,"El total de la suma es:"+sumaT);//bien
+        //mostrarnumero(sumaT);
+        return sumaT;
     }
 
+    //RESTAR N NUMEROS
     public int restar(){
-
-        return 0;
+        int restaT=0;
+        n = Integer.parseInt(JOptionPane.showInputDialog("Ingrese n "));
+        for(int i=0;i<n;i++){
+            //numero=Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero"));
+            numero = ingresarnumero();
+            if(i>0){
+                numero*=-1;
+            }
+            restaT+=numero;
+        }
+        JOptionPane.showMessageDialog(null,"El total de la resta es:"+restaT);
+        return restaT;
     }
-
+    //MULTIPLICAR N NUMEROS
     public int multiplicar(){
+        int multiplicacionT=1;
+        n = Integer.parseInt(JOptionPane.showInputDialog("Ingrese n "));
 
-        return 0;
+        for(int i=0;i<n;i++){
+            //numero=Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero"));
+            numero = ingresarnumero();
+            multiplicacionT=multiplicacionT*numero;
+        }
+        JOptionPane.showMessageDialog(null,"El total de la multiplicacion es:"+multiplicacionT);
+
+        return multiplicacionT;
+    }
+    //DIVISION N NUMEROS
+    public float dividir(){
+        float divisionT=1;
+        float aux;
+        n = Integer.parseInt(JOptionPane.showInputDialog("Ingrese n "));
+        //numero=Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero"));
+        numero = ingresarnumero();
+        aux=numero;
+        divisionT=aux/divisionT;
+        for(int i=1;i<n;i++){
+            //numero=Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero"));
+            numero = ingresarnumero();
+            divisionT=(float)(divisionT/numero);
+        }
+        JOptionPane.showMessageDialog(null,"El total de la division es:"+divisionT);
+
+        return divisionT;
     }
 
-    public int dividir(){
-
-        return 0;
-    }
-
-
+    //GETTER
     public String getMarcaCalculadora() {
         return marcaCalculadora;
     }
@@ -53,7 +100,5 @@ public class Calculadora {
     public int getResultado() {
         return resultado;
     }
-
-
 
 }
