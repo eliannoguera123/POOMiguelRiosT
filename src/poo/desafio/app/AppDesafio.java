aac5f2d0afe54e2deea71c457042a5e7724d91f1
MiguelRiosT <miguelriostangarife@gmail.com>
2021-09-28 19:56:47 -0500

    Desafio calculadora progreso 7:56pm martes

--- src/poo/desafio/app/AppDesafio.java
+++ src/poo/desafio/app/AppDesafio.java
@@ -9,18 +9,22 @@ public class AppDesafio {
     private static final int OPCION_RESTAR = 1;
     private static final int OPCION_MULTIPLICAR = 2;
     private static final int OPCION_DIVIDIR = 3;
+    private static final int OPCION_CHECKBOX = 4;
 
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora("LG");
+        JCheckBox checkBox = new JCheckBox("¿Quieres redondear?");
         int n;
         while(true){
-            int opcionElegida = JOptionPane.showOptionDialog(null, "Elije una opcion",
+            int opcionElegida = JOptionPane.showOptionDialog(null, "¿Que operación deseas hacer?",
                     "Calculadora ", 0, 0, icono,
-                    Arrays.asList("Sumar", "Restar", "Multiplicar", "Dividir").toArray(), null);
+                    Arrays.asList("Sumar", "Restar", "Multiplicar", "Dividir",checkBox).toArray(), null);
             switch (opcionElegida) {
                 case OPCION_SUMAR :
+
                     n = calculadora.ingresarn();
                     calculadora.sumar(n);
+
                     break;
                 case OPCION_RESTAR :
                     n = calculadora.ingresarn();
@@ -33,6 +37,11 @@ public class AppDesafio {
                 case OPCION_DIVIDIR :
                     n = calculadora.ingresarn();
                     calculadora.dividir(n);
+                    break;
+
+                case OPCION_CHECKBOX :
+                    boolean check = checkBox.isSelected();
+
                     break;
                 case JOptionPane.CLOSED_OPTION:
                     return;
